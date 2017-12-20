105e070d9df49205b3cde0ed2c616990f6c02d7d
Sathira Kittisukmongkol <nong.jamie.jamie@gmail.com>
2017-12-21 01:29:09 +0700

    Day5 First Commit

---
+++ Day5/index.js
@@ -0,0 +1,172 @@
+$( () => {
+  let r = 0;
+  let g = 0;
+  let b = 0;
+
+  const displayRGB = (myArray)=> {
+    $('#r-value').val(r);
+    $('#g-value').val(g);
+    $('#b-value').val(b);
+  };
+
+  const displayHSL = (myArray)=> {
+    $('#hsl-h-value').val(myArray[0]);
+    $('#hsl-s-value').val(myArray[1]);
+    $('#hsl-l-value').val(myArray[2]);
+  };
+
+  const displayHSV = (myArray)=> {
+    $('#hsv-h-value').val(myArray[0]);
+    $('#hsv-s-value').val(myArray[1]);
+    $('#hsv-v-value').val(myArray[2]);
+  };
+
+  $('#r-value').on( 'input' , () => {
+    let temp = parseFloat( $('#r-value').val() );
+    if( (temp >= 0) && (temp <= 255) ) {
+      r = temp;
+      let myArray = rgbToHsl(r, g, b);
+      let myArray2 = rgbToHsv(r, g, b);
+      displayHSL(myArray);
+      displayHSV(myArray2);
+      $('#color-field').css( 'background-color' , `rgb(${r},${g},${b})` );
+    }
+  } );
+
+  $('#g-value').on( 'input' , () => {
+    let temp = parseFloat( $('#g-value').val() );
+    if( (temp >= 0) && (temp <= 255) ) {
+      g = temp;
+      let myArray = rgbToHsl(r, g, b);
+      let myArray2 = rgbToHsv(r, g, b);
+      displayHSL(myArray);
+      displayHSV(myArray2);
+      $('#color-field').css( 'background-color' , `rgb(${r},${g},${b})` );
+    }
+  } );
+
+  $('#b-value').on( 'input' , () => {
+    let temp = parseFloat( $('#b-value').val() );
+    if( (temp >= 0) && (temp <= 255) ) {
+      b = temp;
+      let myArray = rgbToHsl(r, g, b);
+      let myArray2 = rgbToHsv(r, g, b);
+      displayHSL(myArray);
+      displayHSV(myArray2);
+      $('#color-field').css( 'background-color' , `rgb(${r},${g},${b})` );
+    }
+  } );
+
+  $('#hsl-h-value').on( 'input' , () => {
+    let tempH = parseFloat( $('#hsl-h-value').val() );
+    let tempS = parseFloat( $('#hsl-s-value').val() );
+    let tempL = parseFloat( $('#hsl-l-value').val() );
+    if( (tempH >= 0) && (tempH <= 1) ) {
+      let myArray = hslToRgb(tempH, tempS, tempL);
+      r = Math.floor( myArray[0] );
+      g = Math.floor( myArray[1] );
+      b = Math.floor( myArray[2] );
+      displayRGB( myArray );
+      let myArray2 = rgbToHsv(r, g, b);
+      displayHSV(myArray2);
+      $('#color-field').css( 'background-color' , `rgb(${r},${g},${b})` );
+    }
+  } );
+
+  $('#hsl-s-value').on( 'input' , () => {
+    let tempH = parseFloat( $('#hsl-h-value').val() );
+    let tempS = parseFloat( $('#hsl-s-value').val() );
+    let tempL = parseFloat( $('#hsl-l-value').val() );
+    if( (tempS >= 0) && (tempS <= 1) ) {
+      let myArray = hslToRgb(tempH, tempS, tempL);
+      r = Math.floor( myArray[0] );
+      g = Math.floor( myArray[1] );
+      b = Math.floor( myArray[2] );
+      displayRGB( myArray );
+      let myArray2 = rgbToHsv(r, g, b);
+      displayHSV(myArray2);
+      $('#color-field').css( 'background-color' , `rgb(${r},${g},${b})` );
+    }
+  } );
+
+  $('#hsl-l-value').on( 'input' , () => {
+    let tempH = parseFloat( $('#hsl-h-value').val() );
+    let tempS = parseFloat( $('#hsl-s-value').val() );
+    let tempL = parseFloat( $('#hsl-l-value').val() );
+    if( (tempL >= 0) && (tempL <= 1) ) {
+      let myArray = hslToRgb(tempH, tempS, tempL);
+      r = Math.floor( myArray[0] );
+      g = Math.floor( myArray[1] );
+      b = Math.floor( myArray[2] );
+      displayRGB( myArray );
+      let myArray2 = rgbToHsv(r, g, b);
+      displayHSV(myArray2);
+      $('#color-field').css( 'background-color' , `rgb(${r},${g},${b})` );
+    }
+  } );
+
+  $('#hsv-h-value').on( 'input' , () => {
+    let tempH = parseFloat( $('#hsv-h-value').val() );
+    let tempS = parseFloat( $('#hsv-s-value').val() );
+    let tempV = parseFloat( $('#hsv-v-value').val() );
+    if( (tempH >= 0) && (tempH <= 1) ) {
+      let myArray = hsvToRgb(tempH, tempS, tempV);
+      r = Math.floor( myArray[0] );
+      g = Math.floor( myArray[1] );
+      b = Math.floor( myArray[2] );
+      displayRGB( myArray );
+      let myArray2 = rgbToHsl(r, g, b);
+      displayHSL(myArray2);
+      $('#color-field').css( 'background-color' , `rgb(${r},${g},${b})` );
+    }
+  } );
+
+  $('#hsv-s-value').on( 'input' , () => {
+    let tempH = parseFloat( $('#hsv-h-value').val() );
+    let tempS = parseFloat( $('#hsv-s-value').val() );
+    let tempV = parseFloat( $('#hsv-v-value').val() );
+    if( (tempS >= 0) && (tempS <= 1) ) {
+      let myArray = hsvToRgb(tempH, tempS, tempV);
+      r = Math.floor( myArray[0] );
+      g = Math.floor( myArray[1] );
+      b = Math.floor( myArray[2] );
+      displayRGB( myArray );
+      let myArray2 = rgbToHsl(r, g, b);
+      displayHSL(myArray2);
+      $('#color-field').css( 'background-color' , `rgb(${r},${g},${b})` );
+    }
+  } );
+
+  $('#hsv-v-value').on( 'input' , () => {
+    let tempH = parseFloat( $('#hsv-h-value').val() );
+    let tempS = parseFloat( $('#hsv-s-value').val() );
+    let tempV = parseFloat( $('#hsv-v-value').val() );
+    if( (tempV >= 0) && (tempV <= 1) ) {
+      let myArray = hsvToRgb(tempH, tempS, tempV);
+      r = Math.floor( myArray[0] );
+      g = Math.floor( myArray[1] );
+      b = Math.floor( myArray[2] );
+      displayRGB( myArray );
+      let myArray2 = rgbToHsl(r, g, b);
+      displayHSL(myArray2);
+      $('#color-field').css( 'background-color' , `rgb(${r},${g},${b})` );
+    }
+  } );
+
+  $('#clear-butt').on( 'click' , () => {
+    r = 0;
+    g = 0;
+    b = 0;
+    $('#r-value').val(0);
+    $('#g-value').val(0);
+    $('#b-value').val(0);
+    $('#hsl-h-value').val(0)
+    $('#hsl-s-value').val(0)
+    $('#hsl-l-value').val(0)
+    $('#hsv-h-value').val(0)
+    $('#hsv-s-value').val(0)
+    $('#hsv-v-value').val(0)
+    $('#color-field').css( 'background-color' , `rgb(${r},${g},${b})` );
+  } );
+
+} );
