cfb67d67b31bb45a6629c30ea7c5f6d57ca7317b
Sathira Kittisukmongkol <nong.jamie.jamie@gmail.com>
2017-12-21 13:29:14 +0700

    Set default if no query parameters

--- Day4/index.js
+++ Day4/index.js
@@ -1,25 +1,31 @@
 $( () => {
   const query = decodeURIComponent(window.location.search.substring(1));
-  const array = query.split('&');
-  const obj = {};
-  array.forEach((item) => {
-          const keyvalue = item.split('=');
-          obj[keyvalue[0]] = keyvalue[1]
-  });
-  // STR
-  $('#str-percent').html( obj.str );
-  $('#str-percent').css('width' , obj.str*2 + '%');
-  // DEF
-  $('#def-percent').html( obj.def );
-  $('#def-percent').css('width' , obj.def*2 + '%');
-  // INT
-  $('#int-percent').html( obj.int );
-  $('#int-percent').css('width' , obj.int*2 + '%');
-  // LUX
-  $('#lux-percent').html( obj.lux );
-  $('#lux-percent').css('width' , obj.lux*2 + '%');
 
-  $('#return-butt').on( 'click' , () => {
+  if( query.length == 0 ) {
     window.location = '../Day2/index.html';
-  } );
+  }
+  else {
+    const array = query.split('&');
+    const obj = {};
+    array.forEach((item) => {
+            const keyvalue = item.split('=');
+            obj[keyvalue[0]] = keyvalue[1]
+    });
+    // STR
+    $('#str-percent').html( obj.str );
+    $('#str-percent').css('width' , obj.str*2 + '%');
+    // DEF
+    $('#def-percent').html( obj.def );
+    $('#def-percent').css('width' , obj.def*2 + '%');
+    // INT
+    $('#int-percent').html( obj.int );
+    $('#int-percent').css('width' , obj.int*2 + '%');
+    // LUX
+    $('#lux-percent').html( obj.lux );
+    $('#lux-percent').css('width' , obj.lux*2 + '%');
+
+    $('#return-butt').on( 'click' , () => {
+      window.location = '../Day2/index.html';
+    } );
+  }
 } );
